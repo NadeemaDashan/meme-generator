f1feb6b117eb572627375257476e8d8a715749a5
Dashan Nadeema <dashanwd@gmail.com>
2024-02-06 13:50:27 +0530

    real time text on image update in form completed

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -4,27 +4,52 @@ import memesData from "./memesData.js";
 
 export default function Form(){
 
-    const [memeImage,setMemeImage]=React.useState("http://i.imgflip.com/1bij.jpg");
+    const [formData,setFormData]=React.useState({
+        topText:"",
+        bottomText:"",
+        image:"http://i.imgflip.com/1bij.jpg"
+    });
 
-    function getNewMemeImage(){
+    function getNewMemeImage(event){
+        event.preventDefault();
         let memeArray=memesData.data.memes;
         let randomIndex=Math.floor(Math.random()*memeArray.length+1);
         console.log(randomIndex);
-        setMemeImage(memeArray[randomIndex].url);
+        setFormData(prevState => {
+            return{
+                ...prevState,
+                image:[memeArray[randomIndex].url]
+            }
+        });
     }
+    function handleChange(event){
+        setFormData(prevState=>{
+            return{
+                ...prevState,
+                [event.target.name]:[event.target.value]
+            }
+        })
+    } 
+    console.log(formData)
 
     return(
         <main>
-            <div className="form">
+            <form className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
                     className="form-input"
+                    name="topText"
+                    value={formData.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form-input"
+                    name="bottomText"
+                    value={formData.bottomText}
+                    onChange={handleChange}
                 />
                 <button onClick={getNewMemeImage}
                     className="form-button"
@@ -32,8 +57,14 @@ export default function Form(){
                 >
                     Get a new meme image 🖼
                 </button>
-                <img src={memeImage} className="meme-image" />
-            </div>
+                <div className="output-content">
+                <div className="meme">
+                <img id="meme-img" src={formData.image} className="meme-image" />
+                <h2 className="meme-text top">{formData.topText}</h2>
+                <h2 className="meme-text bottom">{formData.bottomText}</h2>
+                </div>
+                </div>
+            </form>
         </main>
     )
 }
