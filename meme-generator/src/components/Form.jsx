483a3daa96a7da47ddddf529855a0b0855088a2c
Dashan Nadeema <dashanwd@gmail.com>
2024-02-06 20:38:45 +0530

    removed the array and added the direct fetch from api

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -1,41 +1,51 @@
 import React from "react";
 import './Form.css'
-import memesData from "./memesData.js";
 
-export default function Form(){
+export default function Form() {
 
-    const [formData,setFormData]=React.useState({
-        topText:"",
-        bottomText:"",
-        image:"http://i.imgflip.com/1bij.jpg"
+    const [formData, setFormData] = React.useState({
+        topText: "",
+        bottomText: "",
+        image: ""
     });
 
-    function getNewMemeImage(event){
+    const [allMemes, setAllMemes] = React.useState([]);
+
+    function getNewMemeImage(event) {
         event.preventDefault();
-        let memeArray=memesData.data.memes;
-        let randomIndex=Math.floor(Math.random()*memeArray.length+1);
-        console.log(randomIndex);
+        let randomIndex = Math.floor(Math.random() * allMemes.length + 1);
+        let url = allMemes[randomIndex].url
         setFormData(prevState => {
-            return{
+            return {
                 ...prevState,
-                image:[memeArray[randomIndex].url]
+                image: url
             }
         });
     }
-    function handleChange(event){
-        setFormData(prevState=>{
-            return{
+    function handleChange(event) {
+        setFormData(prevState => {
+            return {
                 ...prevState,
-                [event.target.name]:[event.target.value]
+                [event.target.name]: [event.target.value]
             }
         })
-    } 
+    }
     console.log(formData)
 
-    return(
+    React.useEffect(() => {
+        async function getMemeData() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json();
+            setAllMemes(data.data.memes)
+
+
+        }
+        getMemeData();
+    }, [])
+    return (
         <main>
             <form className="form">
-                <input 
+                <input
                     type="text"
                     placeholder="Top text"
                     className="form-input"
@@ -43,7 +53,7 @@ export default function Form(){
                     value={formData.topText}
                     onChange={handleChange}
                 />
-                <input 
+                <input
                     type="text"
                     placeholder="Bottom text"
                     className="form-input"
@@ -53,16 +63,16 @@ export default function Form(){
                 />
                 <button onClick={getNewMemeImage}
                     className="form-button"
-                    
+
                 >
                     Get a new meme image 🖼
                 </button>
                 <div className="output-content">
-                <div className="meme">
-                <img id="meme-img" src={formData.image} className="meme-image" />
-                <h2 className="meme-text top">{formData.topText}</h2>
-                <h2 className="meme-text bottom">{formData.bottomText}</h2>
-                </div>
+                    <div className="meme">
+                        <img id="meme-img" src={formData.image} className="meme-image" />
+                        <h2 className="meme-text top">{formData.topText}</h2>
+                        <h2 className="meme-text bottom">{formData.bottomText}</h2>
+                    </div>
                 </div>
             </form>
         </main>
